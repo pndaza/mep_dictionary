1b64e132ed0b4945b612d10bddacafb0efb780ca
pndaza <mrpndaza@outlook.com>
2022-07-01 12:12:01 +0630

    difine macos window size

--- lib/screen/home/widgets/definition_list_view.dart
+++ lib/screen/home/widgets/definition_list_view.dart
@@ -3,18 +3,40 @@ import 'package:flutter/material.dart';
 import '../../../model/definition.dart';
 import 'definition_list_tile.dart';
 
-class DefinitionListView extends StatelessWidget {
+class DefinitionListView extends StatefulWidget {
   final List<Definition> definitions;
   const DefinitionListView({Key? key, required this.definitions})
       : super(key: key);
 
   @override
+  State<DefinitionListView> createState() => _DefinitionListViewState();
+}
+
+class _DefinitionListViewState extends State<DefinitionListView> {
+  late final ScrollController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: definitions.length,
-      itemBuilder: (context, index) {
-        return DefinitionListTile(definition: definitions[index]);
-      },
+       return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView.builder(
+        // controller: controller,
+        itemCount: widget.definitions.length,
+        itemBuilder: (context, index) {
+          return DefinitionListTile(definition: widget.definitions[index]);
+        },
+      ),
     );
   }
 }
